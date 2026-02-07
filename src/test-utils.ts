b98fee77d402ab1db2551ae0f58a15e4e7ec6235
Philip Jackson <p-jackson@live.com>
2026-02-08 00:22:23 +1300

    Use file-based routing in test-utils
    Tests real expo-router discovery, close to production behavior

--- src/test-utils.ts
+++ src/test-utils.ts
@@ -1,17 +1,9 @@
+import path from "path";
 import {
   renderRouter,
   type RenderRouterOptions,
 } from "expo-router/testing-library";
-import Home from "./app/index";
-import History from "./app/history";
-import RootLayout from "./app/_layout";
-
-export const APP_ROUTES = {
-  index: Home,
-  history: History,
-  _layout: RootLayout,
-};
 
 export function renderApp(options: RenderRouterOptions) {
-  return renderRouter(APP_ROUTES, options);
+  return renderRouter(path.resolve(__dirname, "app"), options);
 }
